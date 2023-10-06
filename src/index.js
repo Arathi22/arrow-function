cb35bbfb98aeeb3313e8ba12198bdc031e677f14
Arathi22 <arathirajagopalan22@gmail.com>
2023-10-06 13:03:39 +0530

    react-2

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import App from './components/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//arrow function.............................................
+
+// var numbers = [3, 56, 2, 48, 5];
+
+//this is the normal way we do the map function
+// const newNumber = numbers.map(function (x) {
+//     return x * x;
+// });
+
+//using arrow function   ( => )
+// const newNumber = numbers.map((x) => {//(x) it is the input and others are the body of the function
+//     return x * x;
+// });
+
+//using simplified arrow function   ( => ).
+//this is only possible when only one input is there. other waise we have to do the above method.
+// const newNumber = numbers.map(x => x * x);
+
+// console.log(newNumber);
+
+//.......................................................................
+//simplyifing with arrow function
+
+//Map -Create a new array by doing something with each item in an array.
+// const newNumbers = numbers.map(x => x * 2);
+
+////Filter - Create a new array by keeping the items that return true.
+// const newNumbers = numbers.filter(num => num < 10);
+
+// Reduce - Accumulate a value by doing something to each item in an array.
+// var newNumbers = numbers.reduce((accumulator, currentNumber) => accumulator + currentNumber);
+
+//Find - find the first item that matches from an array.
+// const newNumbers = numbers.find(num => num > 10)
+
+//FindIndex - find the index of the first item that matches.
+// const newNumbers = numbers.findIndex(num => num > 10);
+// console.log(newNumbers);
